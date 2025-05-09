ca245a672ad147262611d64d5c62adf9669ebb2b
Alex Jeman <lxjeman@gmail.com>
2025-05-09 12:25:16 +0300

    new file:   TODO 	new file:   script/api/database/runtime.php 	new file:   script/api/paths.php 	new file:   server.sh

---
+++ script/api/paths.php
@@ -0,0 +1,12 @@
+<?php
+$paths = [
+    "root" => $_SERVER("DOCUMENT_ROOT"),
+    "pages" => $_SERVER["DOCUMENT_ROOT"] . "/pages",
+    "assets" => $_SERVER["DOCUMENT_ROOT"] . "/assets",
+    "scriptsAPI_DATABASE" => $_SERVER["DOCUMENT_ROOT"] . "/script/api/database",
+    "scriptsAPI" => $_SERVER["DOCUMENT_ROOT"] . "/script/api",
+    "scriptsClient" => $_SERVER["DOCUMENT_ROOT"] . "/script/client",
+    "scriptsServer" => $_SERVER["DOCUMENT_ROOT"] . "/script/server",
+    "styles" => $_SERVER["DOCUMENT_ROOT"] . "/styles",
+]
+?>
